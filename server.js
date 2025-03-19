aa9e91f39641533c095b51e0b04db03680399160
Saul De la fuente <saul.delafuenteb@gmail.com>
2025-03-19 12:30:55 -0600

    NUevos cambios para verificar cors

--- server.js
+++ server.js
@@ -13,11 +13,21 @@ const port =process.env.PORT || 3000;
 const app = express();
 app.use(express.json())
 app.use(cors({
-  origin: 'https://pf.siradiacion.com.mx',
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
+// 🔹 Responde manualmente a solicitudes OPTIONS
+app.options('*', cors());
+
+// 🔹 Añade encabezados a todas las respuestas
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 const httpServer = createServer(app)
 
 //Socket.io
