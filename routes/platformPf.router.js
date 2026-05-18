3aa581badbdee826a86eadc9c494d2048ad46909
Saul De la fuente <saul.delafuenteb@gmail.com>
2026-05-18 10:36:33 -0600

    Antiguo forma de sacar documentos desde firebase

--- routes/platformPf.router.js
+++ routes/platformPf.router.js
@@ -8,7 +8,6 @@ const reports = new Reports()
 router.get('/:year/:id',async(req,res)=>{
   const { year,id } = req.params
 
-
     const get = await reports.getAll(year,id)
     res.status(get.status).json(get)
 
